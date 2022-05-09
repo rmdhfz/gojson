e07d8304c3675872706bd9a175ae9defc2cb96b0
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-05-09 14:56:07 +0700

    add: TestEncode

---
+++ encode_json_test.go
@@ -0,0 +1,21 @@
+package gojson
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func logJson(data interface{}) {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bytes))
+}
+func TestEncode(t *testing.T) {
+	logJson("Hafiz")
+	logJson("1")
+	logJson(true)
+	logJson([]string{"Hafiz", "Ramadhan"})
+}
